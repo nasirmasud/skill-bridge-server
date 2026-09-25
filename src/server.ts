0ec1fa2c3ff992dce6f32b13d93640daeed52f5c
nasirmasud <nasir.masud@ymail.com>
2026-09-25 17:15:28 +0600

    feat(server): start the keep-alive pinger on boot

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import app from "./app";
 import { env } from "./config/env";
+import { startDbKeepAlive } from "./lib/dbKeepAlive";
+import { pool } from "./lib/prisma";
 
 app.listen(env.PORT, () => {
   console.log(`Server running on http://localhost:${env.PORT}`);
+  startDbKeepAlive(pool);
 });
